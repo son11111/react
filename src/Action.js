6d34553f72e4dfdaaea1feeb97261c5e8859408b
Son Phan <vipvipvipp98@gmail.com>
2020-11-10 18:01:39 +0700

    addfileaction

---
+++ src/Action.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Action extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+Action.propTypes = {
+
+};
+
+
+export default Action;
